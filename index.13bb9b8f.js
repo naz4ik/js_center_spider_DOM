3ab31c24829909bb28797ccd066faeb8dede1df6
Nazar Shtefaniuk <nazik200934@gmail.com>
2024-12-02 08:55:51 +0100

    make build

---
+++ index.13bb9b8f.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),o=t.offsetWidth,s=t.offsetHeight,f=e.offsetWidth,l=e.offsetHeight;e.style.position="absolute",e.style.left="".concat((o-f)/2,"px"),e.style.top="".concat((s-l)/2,"px");
+//# sourceMappingURL=index.13bb9b8f.js.map
